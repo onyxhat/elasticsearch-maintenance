591e08524140a09013c06a4905ea209d41ce165d
Isaac Springer <onyxhat@users.noreply.github.com>
2017-03-24 11:16:47 -0500

    Update ES-Maintenance.psm1
    Updated to work with Elasticsearch 5.0+

--- ES-Maintenance.psm1
+++ ES-Maintenance.psm1
@@ -1,233 +1,212 @@
-###Functions
-function New-IndexObject() {
+###Exposed Functions
+function Get-EsIndexes() {
     [CmdletBinding()]
     Param (
-        [string]$Server,
-        [int]$Port,
-        [string[]]$Indexes
+        [ValidateSet("http","https")]
+        [string]$Protocol = "http",
+        [string]$Server = "localhost",
+        [int]$Port = 9200,
+        [string]$IndexPrefix = ".*"
     )
 
-    #Method Definitions
-    $deleteMethod = {
-        if ($this.Status -eq "Online") {
-            Try {
-                $response = Invoke-WebRequest -Method Delete -Uri ("http://" + $this.Server + ":" + $this.Port + "/" + $This.Index)
-
-                if ($response.StatusCode -eq 200) {
-                    Write-Host -ForegroundColor Green -NoNewline "[DELETED]"
-                    $this.Status = "Deleted"
-                } else {
-                    Write-Warning ("Returned " + $response.StatusCode + " response")
-                }
-            }
-
-            Catch {
-                Write-Host -ForegroundColor Red -NoNewline "[ERROR]"
-            }
-        } else {
-            Write-Host -ForegroundColor Yellow -NoNewline "[SKIPPED]"
-        }
+    Begin {
+        $r = Invoke-WebRequest -Method Get -Uri "${Protocol}://${Server}:${Port}/_aliases?pretty=true" -UseBasicParsing
         
-        Write-Host (" - " + $this.Server + "/" + $this.Index)
+        $defaultProperties = @('Name','Age','IsOnline','HasData')
+        $defaultDisplayPropertySet = New-Object System.Management.Automation.PSPropertySet('DefaultDisplayPropertySet',[string[]]$defaultProperties)
+        $PSStandardMembers = [System.Management.Automation.PSMemberInfo[]]@($defaultDisplayPropertySet)
     }
 
-    $optimizeMethod = {
-        if ($this.Status -eq "Online") {
-            Try {
-                $response = Invoke-WebRequest -Method Post -Uri ("http://" + $this.Server + ":" + $this.Port + "/" + $this.Index + "/_optimize")
+    Process {
+        if ($r.StatusCode -eq 200) {
+            $indexes = $r.Content.Split(":`r`n") -replace " ","" -replace """","" | Where-Object { ($_ -match "^\w+") -and ($_ -ne "aliases") } | Select-String $IndexPrefix | Sort-Object
 
-                if ($response.StatusCode -eq 200) {
-                    $results = $($response.Content | ConvertFrom-Json)._shards
+            [psobject[]]$IndexObj = foreach ($i in $indexes) {
+                $o = New-Object -TypeName psobject
+                $o | Add-Member -MemberType NoteProperty -Name Name -Value $i
+                $o | Add-Member -MemberType NoteProperty -Name Server -Value $Server
+                $o | Add-Member -MemberType NoteProperty -Name Port -Value $Port
+                $o | Add-Member -MemberType NoteProperty -Name BaseUrl -Value "${Protocol}://${Server}:${Port}"
+                $o | Add-Member -MemberType NoteProperty -Name Output -Value $null
+                $o | Add-Member -MemberType NoteProperty -Name Error -Value $null
+                $o | Add-Member -MemberType ScriptProperty -Name HasData -Value { ($this.Output -ne $null) -or ($this.Error -ne $null) }
+                $o | Add-Member -MemberType ScriptProperty -Name Age -Value { Get-EsIndexAge }
+                $o | Add-Member -MemberType ScriptProperty -Name IsOnline -Value { Get-EsIndexState }
+                $o | Add-Member -MemberType ScriptMethod -Name SetOpen -Value { Set-EsIndexOpen }
+                $o | Add-Member -MemberType ScriptMethod -Name SetClosed -Value { Set-EsIndexClosed }
+                $o | Add-Member -MemberType ScriptMethod -Name Merge -Value { Invoke-EsMerge }
+                $o | Add-Member -MemberType ScriptMethod -Name Shrink -Value { Invoke-EsShrink }
+                $o | Add-Member -MemberType ScriptMethod -Name Flush -Value { Invoke-EsFlush }
+                $o | Add-Member -MemberType ScriptMethod -Name Remove -Value { Remove-EsIndex }
 
-                    if ($results.failed -ne 0) {
-                        Write-Host -ForegroundColor Red -NoNewline "[ERROR]"
-                    } elseif ($results.successful -lt $results.total) {
-                        Write-Host -ForegroundColor DarkYellow -NoNewline "[WARN]"
-                    } else {
-                        Write-Host -ForegroundColor Green -NoNewline "[OPTIMIZED]"
-                    }
-                } else {
-                    Write-Host -ForegroundColor Red -NoNewline "[ERROR]"
-                    $results = ("Returned " + $response.StatusCode + " response")
-                }
-            }
+                $o.PSObject.TypeNames.Insert(0,'ES.Indexes')
+                $o | Add-Member MemberSet PSStandardMembers $PSStandardMembers
 
-            Catch {
-                Write-Host -ForegroundColor Red -NoNewline "[ERROR]"
+                $o
+
             }
         } else {
-            Write-Host -ForegroundColor Yellow -NoNewline "[SKIPPED]"
+            Throw ("$Server returned response [$($r.StatusCode)]")
         }
-
-        Write-Host (" - " + $this.Server + "/" + $this.Index + " ($results)")
     }
 
-    $flushMethod = {
-        if ($this.Status -eq "Online") {
-            Try {
-                $response = Invoke-WebRequest -Method Post -Uri ("http://" + $this.Server + ":" + $this.Port + "/" + $This.Index + "/_flush")
+    End {
+        return $IndexObj
+    }
+}
 
-                if ($response.StatusCode -eq 200) {
-                    Write-Host -ForegroundColor Green -NoNewline "[FLUSHED]"
-                } else {
-                    Write-Warning ("Returned " + $response.StatusCode + " response")
-                }
-            }
-
-            Catch {
-                Write-Host -ForegroundColor Red -NoNewline "[ERROR]"
-            }
-        } else {
-            Write-Host -ForegroundColor Yellow -NoNewline "[SKIPPED]"
-        }
-
-        Write-Host (" - " + $this.Server + "/" + $this.Index)
+###Helper Functions
+function Get-EsIndexState() {
+    Try {
+        Invoke-RestMethod -Method Get -Uri "$($this.BaseUrl)/$($this.Name)/_stats" | Out-Null
+        return $true
     }
 
-    $clearCacheMethod = {
-        if ($this.Status -eq "Online") {
-            Try {
-                $response = Invoke-WebRequest -Method Post -Uri ("http://" + $this.Server + ":" + $this.Port + "/" + $This.Index + "/_cache/clear")
-
-                if ($response.StatusCode -eq 200) {
-                    Write-Host -ForegroundColor Green -NoNewline "[CACHE_CLEARED]"
-                } else {
-                    Write-Warning ("Returned " + $response.StatusCode + " response")
-                }
-            }
-
-            Catch {
-                Write-Host -ForegroundColor Red -NoNewline "[ERROR]"
-            }
-        } else {
-            Write-Host -ForegroundColor Yellow -NoNewline "[SKIPPED]"
-        }
-
-        Write-Host (" - " + $this.Server + "/" + $this.Index)
-    }
-
-    $refreshMethod = {
-        if ($this.Status -eq "Online") {
-            Try {
-                $response = Invoke-WebRequest -Method Post -Uri ("http://" + $this.Server + ":" + $this.Port + "/" + $This.Index + "/_refresh")
-
-                if ($response.StatusCode -eq 200) {
-                    Write-Host -ForegroundColor Green -NoNewline "[REFRESHED]"
-                } else {
-                    Write-Warning ("Returned " + $response.StatusCode + " response")
-                }
-            }
-
-            Catch {
-                Write-Host -ForegroundColor Red -NoNewline "[ERROR]"
-            }
-        } else {
-            Write-Host -ForegroundColor Yellow -NoNewline "[SKIPPED]"
-        }
-
-        Write-Host (" - " + $this.Server + "/" + $this.Index)
-    }
-
-    $openMethod = {
-        Try {
-            $response = Invoke-WebRequest -Method Post -Uri ("http://" + $this.Server + ":" + $this.Port + "/" + $This.Index + "/_open")
-
-            if ($response.StatusCode -eq 200) {
-                Write-Host -ForegroundColor Green -NoNewline "[OPENED]"
-                $this.Status = "Online"
-            } else {
-                Write-Warning ("Returned " + $response.StatusCode + " response")
-            }
-        }
-
-        Catch {
-            Write-Host -ForegroundColor Red -NoNewline "[ERROR]"
-        }
-
-        Write-Host (" - " + $this.Server + "/" + $this.Index)
-    }
-
-    $closeMethod = {
-        if ($this.Status -eq "Online") {
-            Try {
-                $response = Invoke-WebRequest -Method Post -Uri ("http://" + $this.Server + ":" + $this.Port + "/" + $This.Index + "/_close")
-
-                if ($response.StatusCode -eq 200) {
-                    Write-Host -ForegroundColor Green -NoNewline "[CLOSED]"
-                    $this.Status = "Offline"
-                } else {
-                    Write-Warning ("Returned " + $response.StatusCode + " response")
-                }
-            }
-
-            Catch {
-                Write-Host -ForegroundColor Red -NoNewline "[ERROR]"
-            }
-        } else {
-            Write-Host -ForegroundColor Yellow -NoNewline "[SKIPPED]"
-        }
-
-        Write-Host (" - " + $this.Server + "/" + $this.Index)
-    }
-
-    #Object Construction
-    foreach ($i in $Indexes) {
-        $objIndex = New-Object -TypeName PSObject
-        
-        $objIndex | Add-Member -MemberType NoteProperty -Name Server -Value $Server
-        $objIndex | Add-Member -MemberType NoteProperty -Name Port -Value $Port
-        $objIndex | Add-Member -MemberType NoteProperty -Name Index -Value $i
-        $objIndex | Add-Member -MemberType NoteProperty -Name Age -Value $(Get-EsIndexAge -Index $i)
-        $objIndex | Add-Member -MemberType NoteProperty -Name Status -Value "Online"
-        $objIndex | Add-Member -MemberType ScriptMethod -Name Delete -Value $deleteMethod
-        $objIndex | Add-Member -MemberType ScriptMethod -Name Optimize -Value $optimizeMethod
-        $objIndex | Add-Member -MemberType ScriptMethod -Name Flush -Value $flushMethod
-        $objIndex | Add-Member -MemberType ScriptMethod -Name ClearCache -Value $clearCacheMethod
-        $objIndex | Add-Member -MemberType ScriptMethod -Name Refresh -Value $refreshMethod
-        $objIndex | Add-Member -MemberType ScriptMethod -Name OpenIndex -Value $openMethod
-        $objIndex | Add-Member -MemberType ScriptMethod -Name CloseIndex -Value $closeMethod
-
-        $objIndex
+    Catch {
+        return $false
     }
 }
 
 function Get-EsIndexAge() {
-    [CmdletBinding()]
-    Param (
-        [Parameter(Mandatory=$true)][string]$Index,
+    Begin {
         [regex]$Pattern="^\w+[-\.](\d+)[-\.](\d+)[-\.](\d+)([-\.](\d+))*"
-    )
+    }
 
-    if ($Index -match $Pattern) {
-        switch($Matches.Count) {
-            4 { $span = $(Get-Date).ToUniversalTime() - $(Get-Date -Year $Matches[1] -Month $Matches[2] -Day $Matches[3]) } #Daily Indexes
-            6 { $span = $(Get-Date).ToUniversalTime() - $(Get-Date -Year $Matches[1] -Month $Matches[2] -Day $Matches[3] -Hour $Matches[5]) } #Hourly Indexes
+    Process {
+        if ($this.Name -match $Pattern) {
+            switch($Matches.Count) {
+                4 { $span = $(Get-Date).ToUniversalTime() - $(Get-Date ($Matches[2] + "/" + $Matches[3] + "/" + $Matches[1])) } #Daily Indexes
+                6 { $span = $(Get-Date).ToUniversalTime() - $(Get-Date ($Matches[2] + "/" + $Matches[3] + "/" + $Matches[1] + " " + $Matches[5] + ":00:00")) } #Hourly Indexes
+            }
         }
+    }
 
+    End {
         return $span
     }
 }
 
-function Get-EsIndexes() {
-    [CmdletBinding()]
-    Param (
-        [string]$Server="localhost",
-        [int]$Port=9200,
-        [string]$IndexPrefix=".*"
-    )
-    Try {
-        $response = Invoke-WebRequest -Method Get -Uri ("http://" + $Server + ":" + $Port + "/_aliases?pretty=true")
+function Remove-EsIndex() {
+    Write-Host "$($this.Name) " -NoNewline
 
-        if ($response.StatusCode -eq 200) {
-            $indexes = $response.Content.Split(":`r`n") -replace " ","" -replace """","" | Where-Object { ($_ -match "^\w+") -and ($_ -ne "aliases") } | Select-String $IndexPrefix | Sort-Object
-        } else {
-            Write-Warning ("Returned " + $response.StatusCode + " response")
+    if ($this.IsOnline) {
+        Try {
+            $this.Output = Invoke-RestMethod -Method Delete -Uri "$($this.BaseUrl)/$($this.Name)"
+            Write-Host -ForegroundColor Green "[DELETED]"
         }
-    }
 
-    Catch {
-        Throw ("Error retrieving indexes: " + $Error[0])
+        Catch {
+            $this.Error = $_.Exception.Message
+            Write-Host -ForegroundColor Red "[ERROR]"
+        }
+    } else {
+        Write-Host -ForegroundColor Yellow "[SKIPPED]"
     }
+}
 
-    New-IndexObject -Server $Server -Port $Port -Indexes $indexes
+function Set-EsIndexOpen() {
+    Write-Host "$($this.Name) " -NoNewline
+
+    if (!$this.IsOnline) {
+        Try {
+            $this.Output = Invoke-RestMethod -Method Post -Uri "$($this.BaseUrl)/$($this.Name)/_open"
+            Write-Host -ForegroundColor Green "[OPENED]"
+        }
+
+        Catch {
+            $this.Error = $_.Exception.Message
+            Write-Host -ForegroundColor Red "[ERROR]"
+        }
+    } else {
+        Write-Host -ForegroundColor Yellow "[SKIPPED]"
+    }
+}
+
+function Set-EsIndexClosed() {
+    Write-Host "$($this.Name) " -NoNewline
+
+    if ($this.IsOnline) {
+        Try {
+            $this.Output = Invoke-RestMethod -Method Post -Uri "$($this.BaseUrl)/$($this.Name)/_close"
+            Write-Host -ForegroundColor Green "[CLOSED]"
+        }
+
+        Catch {
+            $this.Error = $_.Exception.Message
+            Write-Host -ForegroundColor Red "[ERROR]"
+        }
+    } else {
+        Write-Host -ForegroundColor Yellow "[SKIPPED]"
+    }
+}
+
+function Invoke-EsMerge() {
+    Write-Host "$($this.Name) " -NoNewline
+
+    if ($this.IsOnline) {
+        Try {
+            $this.Output = Invoke-RestMethod -Method Post -Uri "$($this.BaseUrl)/$($this.Name)/_forcemerge"
+            Write-Host -ForegroundColor Green "[MERGED]"
+        }
+
+        Catch {
+            $this.Error = $_.Exception.Message
+            Write-Host -ForegroundColor Red "[ERROR]"
+        }
+    } else {
+        Write-Host -ForegroundColor Yellow "[SKIPPED]"
+    }
+}
+
+function Invoke-EsFlush() {
+    Write-Host "$($this.Name) " -NoNewline
+
+    if ($this.IsOnline) {
+        Try {
+            $this.Output = Invoke-RestMethod -Method Post -Uri "$($this.BaseUrl)/$($this.Name)/_flush"
+            Write-Host -ForegroundColor Green "[FLUSHED]"
+        }
+
+        Catch {
+            $this.Error = $_.Exception.Message
+            Write-Host -ForegroundColor Red "[ERROR]"
+        }
+    } else {
+        Write-Host -ForegroundColor Yellow "[SKIPPED]"
+    }
+}
+
+function Invoke-EsShrink() {
+    Write-Host "$($this.Name) " -NoNewline
+
+    if ($this.IsOnline) {
+        Try {
+            Set-EsIndexReadOnly
+            
+            $this.Output = Invoke-RestMethod -Method Post -Uri "$($this.BaseUrl)/$($this.Name)/_shrink/$($this.Name)_shrunk"
+            Write-Host -ForegroundColor Green "[SHRUNK]"
+        }
+
+        Catch {
+            $this.Error = $_.Exception.Message
+            Write-Host -ForegroundColor Red "[ERROR]"
+        }
+    } else {
+        Write-Host -ForegroundColor Yellow "[SKIPPED]"
+    }
+}
+
+function Set-EsIndexReadOnly() {
+    if ($this.IsOnline) {
+        $Settings = @{
+            settings = @{
+                "index.routing.allocation.require._name" = "$($this.Name)_shrunk"
+                "index.blocks.write" = $true
+            }
+        }
+
+        $this.Output = Invoke-RestMethod -Method Put -Uri "$($this.BaseUrl)/$($this.Name)/_shrink/$($this.Name)_shrunk" -Body $Settings -ContentType 'application/json'
+    } else {
+        Throw "Index [$($this.Name)] is offline"
+    }
 }
